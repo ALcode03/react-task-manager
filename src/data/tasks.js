323c810c2eafb544fe4a4b08a0d7e4b5b3c6f3a0
Alexandra KEDJU <alexandrakedju@gmail.com>
2026-06-10 18:04:58 +0200

    data set tasksList

---
+++ src/data/tasks.js
@@ -0,0 +1,19 @@
+const tasks = [
+    {
+        id: 1,
+        title: "Build a Navbar",
+        status: "Complete"
+    },
+      {
+        id: 2,
+        title: "Create a Dashboard",
+        status: "In progress"
+    },
+      {
+        id: 3,
+        title: "Implement Router",
+        status: "Pending"
+    }
+];
+
+export default tasks;
