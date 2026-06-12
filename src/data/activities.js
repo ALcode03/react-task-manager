b7a7ea501ff803c827224249c90323645a49365e
Alexandra KEDJU <alexandrakedju@gmail.com>
2026-06-12 17:07:59 +0200

    Dataset activities created

---
+++ src/data/activities.js
@@ -0,0 +1,10 @@
+const activities = [
+    "Created Dashboard",
+    "Added Router",
+    "Created Admin Panel",
+    "Updated Settings Page",
+    "Fix the Profile Page",
+    "Improve the components",
+];
+
+export default activities;
