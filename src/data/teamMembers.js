15856d1163e71e4e28fdaf6d83e58fe223df744d
Alexandra KEDJU <alexandrakedju@gmail.com>
2026-06-12 16:31:55 +0200

    Dataset team members created

---
+++ src/data/teamMembers.js
@@ -0,0 +1,19 @@
+const memebers = [
+    {
+        id: 1,
+        name: "Tony Starck",
+        role: "Back-end Developer"
+    },
+    {
+        id: 2,
+        name: "Bruce Wayne",
+        role: "Database Administrator"
+    },
+    {
+        id: 3,
+        name: "Clark Kent",
+        role: "Fullstack Developper"
+    },
+];
+
+export default memebers;
