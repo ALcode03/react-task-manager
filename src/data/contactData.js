8e299cdad85466f3686abac1df85065b4e845740
Alexandra KEDJU <alexandrakedju@gmail.com>
2026-06-12 18:02:00 +0200

    Dataset ContactData created

---
+++ src/data/contactData.js
@@ -0,0 +1,10 @@
+const contactData = {
+    CompanyName: "PrimeTek Africa",
+    PhoneNumber: "+243 997 300 904",
+    EmailAddress: "info@primietek.africa",
+    linkedIn: "https://www.linkedin.com/company/primetekafrica"
+
+};
+
+
+export default contactData;
